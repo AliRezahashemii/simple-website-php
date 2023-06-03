ed7dec3d1732af211f8f8dfe98640b12e11ddeb1
Masih <linuxarh@gmail.com>
2023-06-03 17:18:27 +0330

    create basic files and add bootstarp files (js ans css)

---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+	<?php require("templates/head_tag.php"); ?>
+<body>
+
+</body>
+</html>
---
+++ templates/head_tag.php
@@ -0,0 +1,8 @@
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="static/style/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="static/style/style.css">
+	<title>My Store</title>
+</head>
+
