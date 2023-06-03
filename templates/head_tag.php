7affcd985200dc0e74dcfb08734d478593520256
Masih <linuxarh@gmail.com>
2023-06-03 22:35:20 +0330

    change data  header for login user

--- templates/head_tag.php
+++ templates/head_tag.php
@@ -1,6 +1,15 @@
 <head>
 	<?php
 		session_start();
+		if (isset($_SESSION['username'])) {
+			$log_in_user = true;
+			require("db_conf/db_connect.php");
+			mysqli_select_db($conn,"db_store_test");
+			$sql = "SELECT * FROM users WHERE username = '".$_SESSION['username']."'";
+			$result = mysqli_query($conn,$sql);
+			$info = mysqli_fetch_all($result, MYSQLI_ASSOC)[0];
+			$username_title = $info["username"];
+		}
 	?>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
--- templates/header.php
+++ templates/header.php
@@ -1,14 +1,23 @@
 <header class="container border bg-light rounded">
 	<div class="row p-4 d-flex justify-content-around ">
 		<div class="col-md-3 col-12 my-2 p-1 d-flex justify-content-center">
-
-			<a href="login.php" class="btn btn-outline-success mx-1">Login</a>
-			<a href="register.php" class="btn btn-outline-success mx-1">Register</a>
+			<?php if ($log_in_user){ ?>
+				login
+			<?php } else{ ?>
+				<a href="login.php" class="btn btn-outline-success mx-1">Login</a>
+				<a href="register.php" class="btn btn-outline-success mx-1">Register</a>
+			<?php } ?>
 			
 		</div>
 		<div class="col-md-4 col-12 my-2 p-1 text-start">
 			<span>welcome</span>
-			<span class="fs-5">Guest</span>
+			<?php 
+				if ($log_in_user){
+					echo "<span class='fs-5'>$username_title</span>";
+				}else {
+					echo "<span class='fs-5'>Guest</span>";
+				}
+			?>
 		</div>
 		<div class="col-md-3 col-12 my-2 p-1 text-end">
 			<a href="blog.php" class="text-primary mx-1">Blog</a>
