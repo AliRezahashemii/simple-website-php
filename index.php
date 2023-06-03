3c29aabc712b10e03684ca7edfaf73ae1d531eb2
Masih <linuxarh@gmail.com>
2023-06-03 18:03:52 +0330

    create header structur and create page for other content

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 <html>
 	<?php require("templates/head_tag.php"); ?>
 <body>
-
+	<?php require("templates/header.php"); ?>
+	<div class="container bg-light">
+		ok
+	</div>
+	<script src="static/js/bootstrap.bundle.js"></script>
 </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,17 @@
-<header>
-	
-	
+<header class="container border bg-light rounded">
+	<div class="row p-4 d-flex justify-content-around ">
+		<div class="col-md-3 col-12 my-2 p-1 d-flex justify-content-center">
+			<a href="login.php" class="btn btn-outline-success mx-1">Login</a>
+			<a href="register.php" class="btn btn-outline-success mx-1">Register</a>
+		</div>
+		<div class="col-md-4 col-12 my-2 p-1 text-start">
+			<span>welcome</span>
+			<span class="fs-5">Guest</span>
+		</div>
+		<div class="col-md-3 col-12 my-2 p-1 text-end">
+			<a href="blog.php" class="text-primary mx-1">Blog</a>
+			<a href="index.php" class="text-primary mx-1">Home</a>
+			<span class="fs-4 mx-4">LOGO</span>
+		</div>
+	</div>
 </header>
